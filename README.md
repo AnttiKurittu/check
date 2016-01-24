8ea0bea34d05c7f38f68af481c17e898e516b2dc
Antti Kurittu <antti.kurittu@gmail.com>
2016-01-24 18:30:34 +0200

    Cleaned up a bit

--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 
 <p>This is a simple script to pull information about an IP address or domain.</p>
 
+It currently does the following things:
+
+- Query a list of malware addresses for matches<br>
+- Query WHOIS for domain name and IP address<br>
+- Query PassiveDNS from PassiveTotal and VirusTotal<br>
+- Query VirusTotal for matches coming from target address<br>
+- Query spam blocklists for address
+- Query local GeoIP database<br>
+- Ping the host<br>
+- Check common ports for available services<br>
+- Try to retrieve and show HTTP headers from said ports<br>
+
 <p>It is also the <i>first thing I've written in Python</i> so that's what it looks like.</p>
 
 Export your API keys as environmental variables;<br>
--- check.py
+++ check.py
@@ -1,28 +1,6 @@
 #coding=UTF-8
 
-###### check.py ################################################################
-#
-# Depends on:
-# GeoIP (pip install geoip)
-# IPy (pip install IPy)
-# requests (pip install requests)
-# dnspython (https://github.com/rthalley/dnspython)
-# passivetotal (https://github.com/passivetotal/passivetotal_tools/),
-# install the python module from api_helpers
-# required system commands: "whois" and "ping"
-#
-# Export your API keys as environmental variables;
-# VTAPIKEY for VirusTotal API key, PTAPIKEY for PassiveTotal. If not present,
-# those scans will be skipped.
-# add "export VTAPIKEY=yourapikey" to your shell startup script yo export.
-#
-# if you get trouble email me at antti@kurittu.org and I'll try to help!
-# Tested with debian and OS X, I have no idea whether this thing works on
-# Windows.
-#
-# - Antti Kurittu (antti@kurittu.org)
-#
-###### check.py ################################################################
+# See https://github.com/AnttiKurittu/check/ for details.
 
 import os, sys, datetime, socket, urllib, urllib2, json, argparse, webbrowser, subprocess, zipfile, dns.resolver, requests, GeoIP, StringIO
 from passivetotal import PassiveTotal
